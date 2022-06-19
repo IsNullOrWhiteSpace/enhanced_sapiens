6b21e77150f58cba5d3b76ae69f040b60001f63b
IsNullOrWhiteSpace <132670584+IsNullOrWhiteSpace@users.noreply.github.com>
2022-06-19 15:48:35 +0000

    Initialize commit

--- sapiens/src/models/mod.rs
+++ sapiens/src/models/mod.rs
@@ -20,4 +20,45 @@ pub enum Error {
     OpenAIError(#[from] openai::OpenAIError),
     /// No response from the model
     #[error("No response from the model")]
-    NoR
+    NoResponseFromModel,
+    /// The model is not supported
+    #[error("Model not supported: {0}")]
+    ModelNotSupported(String),
+    /// Vertex AI error
+    #[error("Vertex AI error: {0}")]
+    VertexAIError(#[from] gcp_vertex_ai_generative_language::Error),
+    /// Filtered output
+    #[error("Filtered output")]
+    Filtered,
+}
+
+/// Roles in the conversation
+#[derive(Debug, Serialize, Deserialize, Clone, Default, PartialEq)]
+#[serde(rename_all = "lowercase")]
+pub enum Role {
+    /// The system
+    System,
+    /// The user
+    #[default]
+    User,
+    /// The assistant
+    Assistant,
+    /// Function call
+    Function,
+    /// Tool call
+    Tool,
+}
+
+impl Display for Role {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Role::System => write!(f, "system"),
+            Role::User => write!(f, "user"),
+            Role::Assistant => write!(f, "assistant"),
+            Role::Function => write!(f, "function"),
+            Role::Tool => write!(f, "tool"),
+        }
+    }
+}
+
+// FUTURE(ssoudan) support pure
