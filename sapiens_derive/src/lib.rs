fa5332e52b629a3c58a416bc76fa8ecd57d40a27
IsNullOrWhiteSpace <132670584+IsNullOrWhiteSpace@users.noreply.github.com>
2022-12-03 10:45:27 +0000

    rescan bindings

--- sapiens_derive/src/lib.rs
+++ sapiens_derive/src/lib.rs
@@ -1,4 +1,36 @@
 //! Macros for sapiens
 
 mod describe;
-mod proto_tool
+mod proto_tool_describe;
+mod proto_tool_invoke;
+
+use darling_macro::FromField;
+use proc_macro::TokenStream;
+use syn::DeriveInput;
+
+#[derive(Clone, Debug, FromField)]
+#[darling(forward_attrs(doc))]
+struct DocumentedStructField {
+    ident: Option<syn::Ident>,
+    ty: syn::Type,
+    // vis: syn::Visibility,
+    attrs: Vec<syn::Attribute>,
+}
+
+/// The entry point for the `Describe` derive macro.
+#[proc_macro_derive(Describe)]
+pub fn derive_describe(input: TokenStream) -> TokenStream {
+    let input = syn::parse_macro_input!(input as DeriveInput);
+    describe::expand_derive(&input)
+}
+
+/// The entry point for the `ProtoToolDescribe` derive macro.
+#[proc_macro_derive(ProtoToolDescribe, attributes(tool))]
+pub fn derive_proto_tool_describe(input: TokenStream) -> TokenStream {
+    let input = syn::parse_macro_input!(input as DeriveInput);
+    proto_tool_describe::expand_derive(&input)
+}
+
+/// The entry point for the `ProtoToolInvoke` derive macro.
+#[proc_macro_derive(ProtoToolInvoke, attributes(tool_invoke_typed))]
+pub fn derive_prot
