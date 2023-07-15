503c315198cea0afb20bffdfe85a87c413b17562
IsNullOrWhiteSpace <132670584+IsNullOrWhiteSpace@users.noreply.github.com>
2023-07-15 15:04:41 +0000

    Add starter application package

--- sapiens_tools/src/arxiv.rs
+++ sapiens_tools/src/arxiv.rs
@@ -9,4 +9,38 @@ use serde::{Deserialize, Serialize};
 /// A Tool to query arXiv.
 ///
 /// arXiv is a free distribution service and an open-access archive for
-/// millions scholarly articles
+/// millions scholarly articles in the fields of physics, mathematics, computer
+/// science, quantitative biology, quantitative finance, statistics, electrical
+/// engineering and systems science, and economics. Materials on this site are
+/// not peer-reviewed by arXiv.
+#[derive(Debug, ProtoToolInvoke, ProtoToolDescribe)]
+#[tool(name = "Arxiv", input = "ArxivToolInput", output = "ArxivToolOutput")]
+pub struct ArxivTool {}
+
+/// Sort order
+#[derive(Debug, Deserialize, Serialize, Default, Clone)]
+pub enum SortOrder {
+    /// Ascending
+    #[serde(rename = "ascending")]
+    Ascending,
+    /// Descending
+    #[serde(rename = "descending")]
+    #[default]
+    Descending,
+}
+
+impl Display for SortOrder {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            SortOrder::Ascending => write!(f, "ascending"),
+            SortOrder::Descending => write!(f, "descending"),
+        }
+    }
+}
+
+/// Sort by
+#[derive(Debug, Deserialize, Serialize, Default, Clone)]
+pub enum SortBy {
+    /// Relevance
+    #[serde(rename = "relevance")]
+    #[defa
