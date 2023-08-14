e3b22e96ac200e53450d96c366b3a2797c011264
IsNullOrWhiteSpace <132670584+IsNullOrWhiteSpace@users.noreply.github.com>
2023-08-14 21:40:18 +0000

    change repo name

--- sapiens_tools/src/search/mod.rs
+++ sapiens_tools/src/search/mod.rs
@@ -8,4 +8,41 @@ use tokio::sync::Mutex;
 use tracing::error;
 
 use crate::search::gce::SearchResultNumber::Four;
-use crate::search::gce::
+use crate::search::gce::{ErrorBody, Item, Lr, QueryParameters, SearchResults};
+
+/// A Tool to search the web - powered by Google Custom Search Engine.
+#[cfg(feature = "search")]
+pub mod gce;
+
+/// A Tool to search the web - powered by Google Custom Search Engine.
+///
+/// Returns a list of [`Item`] with: `title`, `link`, `snippet`.
+#[derive(ProtoToolInvoke, ProtoToolDescribe)]
+#[tool(
+    name = "Search",
+    input = "SearchToolInput",
+    output = "SearchToolOutput"
+)]
+pub struct SearchTool {
+    /// API key to use
+    api_key: String,
+    /// CSE ID to use
+    cse_id: String,
+    /// HTTP client
+    client: Mutex<Client>,
+}
+
+impl Debug for SearchTool {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("SearchTool").finish()
+    }
+}
+
+/// [`SearchTool`] input
+#[derive(Debug, Deserialize, Serialize, Describe)]
+pub struct SearchToolInput {
+    /// query to search. `q` parameter of the Google Custom Search Engine API
+    /// Use `exclude_terms` and `exact_terms` to refine your search.
+    q: String,
+
+    /// word or phrase t
