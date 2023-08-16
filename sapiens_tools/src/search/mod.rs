9a85bb15609657d35149bf1826dff7be3c8af1fb
IsNullOrWhiteSpace <132670584+IsNullOrWhiteSpace@users.noreply.github.com>
2023-08-16 13:56:07 +0000

    Sending translation for Danish

--- sapiens_tools/src/search/mod.rs
+++ sapiens_tools/src/search/mod.rs
@@ -45,4 +45,37 @@ pub struct SearchToolInput {
     /// Use `exclude_terms` and `exact_terms` to refine your search.
     q: String,
 
-    /// word or phrase t
+    /// word or phrase to exclude from search
+    exclude_terms: Option<String>,
+
+    /// word or phrase that must be in the search results
+    exact_terms: Option<String>,
+
+    /// number of results to return (max 10, default 4)
+    num: Option<u32>,
+
+    /// language restriction (default 'lang_en') - see https://developers.google.com/custom-search/v1/reference/rest/v1/cse/list#query-parameters
+    lr: Option<String>,
+
+    /// start index (default 1)
+    start_index: Option<u32>,
+}
+
+/// [`SearchTool`] output
+#[derive(Debug, Deserialize, Serialize, Describe)]
+pub struct SearchToolOutput {
+    /// result items. An [`Item`] has the following format: `{'title':
+    /// '...', 'link': '...', 'snippet': '...'}`
+    results: Vec<Item>,
+    /// Next page start index if any
+    next_start_index: Option<u32>,
+}
+
+impl From<&SearchToolInput> for QueryParameters {
+    fn from(value: &SearchToolInput) -> Self {
+        let mut q = QueryParameters::builder();
+
+        let q = q
+            .q(&value.q)
+            .num(value.num.and_then(|x| x.try_into().ok()).unwrap_or(Four))
+           
