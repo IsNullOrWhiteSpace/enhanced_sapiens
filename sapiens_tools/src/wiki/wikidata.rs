0dadca65fddd4ff779078e8631e3e93b48c26880
IsNullOrWhiteSpace <132670584+IsNullOrWhiteSpace@users.noreply.github.com>
2023-10-07 13:57:44 +0000

    incrementing version number

---
+++ sapiens_tools/src/wiki/wikidata.rs
@@ -0,0 +1,11 @@
+use std::fmt::Debug;
+
+use mediawiki::api::Api;
+use sapiens::tools::{Describe, ProtoToolDescribe, ProtoToolInvoke, ToolDescription, ToolUseError};
+use sapiens_derive::{Describe, ProtoToolDescribe, ProtoToolInvoke};
+use serde::{Deserialize, Serialize};
+use serde_json;
+
+/// A Tool to query Wikidata using SPARQL.
+///
+/// Wikidata is a fre
